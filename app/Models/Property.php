26b908854f052d8415577876c78d9ce7a01be0a1
Ezichi Ebere <tripleee@yahoo.co.uk>
2022-02-24 06:46:28 +0100

    Added new fields to property. Added relationship to propertytype.

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Property extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'address', 'property_type_id', 'fields'];
+
+    /** Relationships */
+    public function propertyType(): BelongsTo
+    {
+        return $this->belongsTo(PropertyType::class);
+    }
 }
--- database/migrations/2022_02_24_051835_create_properties_table.php
+++ database/migrations/2022_02_24_051835_create_properties_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('properties', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->foreignId('property_type_id');
+            $table->json('fields');
             $table->timestamps();
         });
     }
