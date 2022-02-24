4b52002c4fc2091a33c9e54a4b22b9ce71d331cf
Ezichi Ebere <tripleee@yahoo.co.uk>
2022-02-24 13:24:49 +0100

    Added properties relationship.

--- app/Models/PropertyType.php
+++ app/Models/PropertyType.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class PropertyType extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    /** Relationships */
+    public function properties(): HasMany
+    {
+        return $this->hasMany(Property::class);
+    }
 }
